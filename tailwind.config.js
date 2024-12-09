31d1c9b7245f7d7d0d8bae87a574cf221d31a0c2
MohamedGhassen <mohamedghassen.hammami@polytechnicien.tn>
2024-12-09 18:33:42 +0100

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        "main-color": "#21232C",
+        "secondary-color": "#2F3137",
+        eps: "#006199",
+      },
+    },
+  },
+  plugins: [],
+};
